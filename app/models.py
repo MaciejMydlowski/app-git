76bcfe022f87c3124472074016c362857c80aacd
Dom <maciejmydel@gmail.com>
2024-06-02 16:00:26 +0200

    dodane RATE do ilości ramek

--- app/models.py
+++ app/models.py
@@ -25,18 +25,27 @@ class Lokalizacja(models.Model):
         return self.miejsce
 
 class PosiadaneUle(models.Model):
+    RATES = (
+        ('6', '6'),
+        ('7', '7'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10'),
+        ('11', '11'),
+        ('12', '12'),
+    )
+
     miejsce = models.ManyToManyField(Lokalizacja)
     nazwa = models.CharField(max_length=15)  
     install_date = models.DateField('data postawienia')
     matka_date = models.DateField('data podania matki')
     matka = models.ManyToManyField(MatkiPszczele)
     typ_ula = models.ManyToManyField(TypUla)
-    ilosc_ramek = models.DecimalField( max_digits=15, decimal_places=0)
+#    ilosc_ramek = models.DecimalField( max_digits=15, decimal_places=0)
+    ilosc_ramek = models.CharField(max_length=2, choices=RATES)
     aktywny = models.BooleanField()
-
+    #aktywny = models.CharField(max_length=1, choices=RATES)
     def publish(self):
         self.save()
     def __str__(self):
         return self.nazwa
-
-
